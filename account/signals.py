da406c83eec6cb790062c9fc7f7623c41ea031c1
Code-Warlock <dessymichaeltgp123@gmail.com>
2022-12-31 17:37:13 +0100

    start paypal integration

--- account/signals.py
+++ account/signals.py
@@ -10,6 +10,6 @@ def create_profile(sender, instance, created, *args, **kwargs):
         SocialHandle.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def save_profile(sender, instance, **kwargs):
+#     instance.profile.save()
